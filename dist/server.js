bf387ae99587f78a930d8cb02351a3a048d3a3ff
Pedro Júnior <pedro_alv12@hotmail.com>
2024-04-23 20:40:31 -0300

    try6

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,8 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-require('dotenv').config();
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 _app2.default.listen(3001, () => {
   console.log(`Servidor rodando na porta: ${3001}`);
