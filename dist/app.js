8f28323f6f2cbadc6b5ce5e231d69e028a46883a
Pedro Júnior <pedro_alv12@hotmail.com>
2024-04-23 20:57:42 -0300

    fix .env database

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
 
-_dotenv2.default.config();
+_dotenv2.default.config({ path: _path.resolve.call(void 0, __dirname, '../.env') });
 
 require('./database');
 
