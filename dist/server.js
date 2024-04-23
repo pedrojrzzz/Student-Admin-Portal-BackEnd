a1710396179aaaa4827d52d9447ebaccff6d7e49
Pedro Júnior <pedro_alv12@hotmail.com>
2024-04-23 20:28:25 -0300

    try3

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,5 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-_app2.default.listen(process.env.APP_PORT, () => {
-  console.log(`Servidor rodando na porta: ${process.env.APP_PORT}`);
+_app2.default.listen(3001, () => {
+  console.log(`Servidor rodando na porta: ${3001}`);
 });
